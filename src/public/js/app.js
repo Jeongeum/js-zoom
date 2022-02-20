c8b4c2e87381c75cf981ddd50eb84192f8aa3b3e
jeongeum <wjddma1202@naver.com>
2022-02-20 15:09:34 +0900

    0219 # simple chat with websockets
    frontend

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,50 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+// 브라우저에서는, 해당 주소로 연결
+const socket = new WebSocket(`ws://${window.location.host}`);
 
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+//backend 에서 온 메시지를 받을 수 있다.
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
+
+/*
+//front에서 back으로 메시지를 보낼 수 있음.
+setTimeout(() => {
+  socket.send("hello from the browser!");
+}, 10000);
+*/
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  const li = document.createElement("li");
+  li.innerText = `You: ${input.value}`;
+  messageList.append(li);
+  input.value = "";
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
