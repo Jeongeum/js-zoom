f7f6b6b17b4166189b56479673e0d3af499baa1b
jeongeum <wjddma1202@naver.com>
2022-02-23 16:07:01 +0900

    0222 webRTC
    줌 클론코딩 끝 ccs 추가 필요

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,14 +1,15 @@
 const socket = io();
-
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
-
+const call = document.getElementById("call");
+call.hidden = true;
 let myStream;
 let muted = false;
 let cameraOff = false;
-
+let roomName;
+let myPeerConnection;
 async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
@@ -16,7 +17,7 @@ async function getCameras() {
     const currentCamera = myStream.getVideoTracks()[0];
     cameras.forEach((camera) => {
       const option = document.createElement("option");
-      option.valuie = camera.deviceId;
+      option.value = camera.deviceId;
       option.innerText = camera.label;
       if (currentCamera.label === camera.label) {
         option.selected = true;
@@ -27,19 +28,18 @@ async function getCameras() {
     console.log(e);
   }
 }
-
 async function getMedia(deviceId) {
-  const initialConstraints = {
+  const initialConstrains = {
     audio: true,
     video: { facingMode: "user" },
   };
   const cameraConstraints = {
-    autio: true,
+    audio: true,
     video: { deviceId: { exact: deviceId } },
   };
   try {
     myStream = await navigator.mediaDevices.getUserMedia(
-      deviceId ? cameraConstraints : initialConstraints
+      deviceId ? cameraConstraints : initialConstrains
     );
     myFace.srcObject = myStream;
     if (!deviceId) {
@@ -49,8 +49,6 @@ async function getMedia(deviceId) {
     console.log(e);
   }
 }
-getMedia();
-
 function handleMuteClick() {
   myStream
     .getAudioTracks()
@@ -63,7 +61,6 @@ function handleMuteClick() {
     muted = false;
   }
 }
-
 function handleCameraClick() {
   myStream
     .getVideoTracks()
@@ -79,8 +76,91 @@ function handleCameraClick() {
 
 async function handleCameraChange() {
   await getMedia(camerasSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
+// Welcome Form (join a room)
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+async function initCall() {
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  makeConnection();
+}
+async function handleWelcomeSubmit(event) {
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  await initCall();
+  socket.emit("join_room", input.value);
+  roomName = input.value;
+  input.value = "";
+}
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+// Socket Code
+socket.on("welcome", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
+});
+socket.on("offer", async (offer) => {
+  console.log("received the offer");
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, roomName);
+  console.log("sent the answer");
+});
+socket.on("answer", (answer) => {
+  console.log("received the answer");
+  myPeerConnection.setRemoteDescription(answer);
+});
+socket.on("ice", (ice) => {
+  console.log("received candidate");
+  myPeerConnection.addIceCandidate(ice);
+});
+// RTC Code
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
+        ],
+      },
+    ],
+  });
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("track", handleTrack);
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+function handleIce(data) {
+  console.log("sent candidate");
+  socket.emit("ice", data.candidate, roomName);
+}
+// function handleAddStream(data) {
+//   const peerFace = document.getElementById("peersFace");
+//   peerFace.srcObject = data.stream;
+// }
+
+function handleTrack(data) {
+  console.log("handle track");
+  const peerFace = document.querySelector("#peersFace");
+  peerFace.srcObject = data.streams[0];
+}
